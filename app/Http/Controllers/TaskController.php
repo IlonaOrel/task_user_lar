ae07454e579f950711b9fdfa2146f327e4a75ee8
Анна Гошкодер <85807541+AnnH1994@users.noreply.github.com>
2021-07-29 20:58:10 +0300

    CRUD

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Illuminate\Support\Facades\Auth;
+
+class TaskController extends Controller
+{
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+       $tasks = Auth::user()->tasks;
+       return view('tasks.index', [
+           'tasks'=>$tasks,
+       ]);
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('tasks.create');
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name'=>'required|max:255',
+        ]);
+
+        Auth::user()
+            ->tasks()
+            ->create([
+                'name'=>$request->name,
+            ]);
+        return redirect(route('tasks.index'));
+    }
+
+     /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * @param Task $task
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     * @throws \Exception
+     */
+    public function destroy(Task $tasks)
+    {
+        $this->authorize('destroy', $tasks);
+        $tasks->delete();
+        return redirect(route('tasks.index'));
+    }
+}
